bb9e7543b3bbdb82757b98a16a89a3ba62329a4a
Ahmed-Nakib <office.nakib@gmail.com>
2025-10-07 08:53:19 +0600

    practice

--- backend/dist/app/modules/auth/auth.services.d.ts
+++ backend/dist/app/modules/auth/auth.services.d.ts
@@ -1,33 +1,12 @@
 import type { Response } from "express";
 import type { IAuth } from "./Auth.interface.js";
 export declare const AuthServices: {
-    login: (payload: IAuth, res: Response) => Promise<(import("mongoose").Document<unknown, {}, {
-        createdAt: NativeDate;
-        updatedAt: NativeDate;
-    } & {
-        name: string;
-        email: string;
-        password: string;
-        avatar: string;
-        isVerified: boolean;
-        isPremium: boolean;
-    }, {}, {
-        timestamps: true;
-        versionKey: "";
-    }> & {
-        createdAt: NativeDate;
-        updatedAt: NativeDate;
-    } & {
-        name: string;
-        email: string;
-        password: string;
-        avatar: string;
-        isVerified: boolean;
-        isPremium: boolean;
-    } & {
-        _id: import("mongoose").Types.ObjectId;
-    } & {
-        "": number;
-    }) | null>;
+    login: (payload: IAuth, res: Response) => Promise<{
+        name: string | undefined;
+        email: string | undefined;
+        avatar: string | undefined;
+        isVerified: boolean | undefined;
+        isPremium: boolean | undefined;
+    }>;
 };
 //# sourceMappingURL=auth.services.d.ts.map
--- backend/src/app/modules/auth/auth.services.ts
+++ backend/src/app/modules/auth/auth.services.ts
@@ -1,6 +1,7 @@
 import type { Response } from "express";
 import { User } from "../user/user.model.js";
 import type { IAuth } from "./Auth.interface.js"
+import bcrypt from "bcryptjs";
 
 const login = async (payload: IAuth, res: Response) => {
     const {email, password} =payload;
@@ -9,10 +10,27 @@ const login = async (payload: IAuth, res: Response) => {
     if(!isUserExist){
         res.status(400).json({
             status: "error",
-            message: "user doesn't exist"
+            message: "email doesn't match"
         })
     }
-    return isUserExist;
+
+    const isPasswordMatch = await bcrypt.compare(password, (isUserExist?.password as string))
+
+    if (!isPasswordMatch){
+        res.status(400).json({
+            status: "error",
+            message: "password doesn't match"
+        }) 
+    }
+
+    const loginUser = {
+        name: isUserExist?.name,
+        email: isUserExist?.email,
+        avatar: isUserExist?.avatar,
+        isVerified: isUserExist?.isVerified,
+        isPremium: isUserExist?.isPremium
+    }
+    return loginUser;
 }
 
 
